569471545f80b5722df6500024dd2a438c562d36
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-02-17 21:42:34 +0530

    feat: initialize project with Express and set up product and user routes
    - Created package.json with dependencies including Express, Mongoose, and others.
    - Implemented productRouter with CRUD operations for products.
    - Implemented userRouter for user creation and login functionality.

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
+import productRouter from "./routes/productRouter.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+    (req, res, next)=>{
+
+        let token = req.header("Authorization");
+        
+        if(token != null){
+
+            token = token.replace("Bearer ", "");
+            console.log(token);
+            
+            jwt.verify(token, process.env.JWT_SECRET_KEY,
+                (err, decoded)=>{
+                    
+                    if(decoded == null){
+                        res.json(
+                            {
+                                message: "Invalid token! Please login again!"
+                            }
+                        )
+
+                        return;
+
+                    } else {
+                        req.user = decoded;
+                    }
+                }
+            );
+        }
+
+        next(); //if he has token, it will be decoded and added to req.user, if not, it will be ignored and the request will be processed as usual.
+
+    } 
+)
+
+const connectionString = process.env.MONGO_URI;
+
+mongoose.connect(connectionString)
+    .then(
+        ()=>{
+            console.log("Database Connected!");
+        }
+    ).catch(
+        ()=>{
+            console.log("Database Connection Failed!");
+        }
+    )
+;
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+
+app.listen(5000, 
+    ()=> {
+        console.log("Server is running on port 5000!");
+    }
+);
---
+++ models/user.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+            default: 'user',
+        },
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
+        isEmailVarified: {
+            type: Boolean,
+            default: false,
+        },
+        image: {
+            type: String,
+            default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        }
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

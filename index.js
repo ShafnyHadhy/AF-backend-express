6eab30a26dd5cdd575c1007df56a8fccfe1ff3b0
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-04-11 08:34:35 +0530

    fix: change default server port from 5001 to 5000

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.use(errorHandler);
 
 // ==================== START SERVER ====================
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
   });
